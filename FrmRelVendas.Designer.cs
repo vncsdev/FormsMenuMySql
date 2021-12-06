5692e05cc6143fed8e3e981713acea73acb0c8aa
Vinicius <vinicius.gabriel83@yahoo.com>
2021-12-06 11:00:36 -0300

    frm rel vendas inicio

--- FrmRelVendas.Designer.cs
+++ FrmRelVendas.Designer.cs
@@ -29,34 +29,191 @@ namespace _23_09_2021
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRelVendas));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpInicio = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFinal = new System.Windows.Forms.DateTimePicker();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.dgvConsulta = new System.Windows.Forms.DataGridView();
             this.btnFechar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnGrafico = new System.Windows.Forms.Button();
+            this.chartVendas = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartVendas)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConsultar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dtpFinal);
+            this.groupBox1.Controls.Add(this.dtpInicio);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 54);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Período:";
+            // 
+            // dtpInicio
+            // 
+            this.dtpInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpInicio.Location = new System.Drawing.Point(64, 19);
+            this.dtpInicio.Name = "dtpInicio";
+            this.dtpInicio.Size = new System.Drawing.Size(98, 20);
+            this.dtpInicio.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "á:";
+            // 
+            // dtpFinal
+            // 
+            this.dtpFinal.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFinal.Location = new System.Drawing.Point(190, 19);
+            this.dtpFinal.Name = "dtpFinal";
+            this.dtpFinal.Size = new System.Drawing.Size(98, 20);
+            this.dtpFinal.TabIndex = 1;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(294, 13);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 32);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // dgvConsulta
+            // 
+            this.dgvConsulta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsulta.Location = new System.Drawing.Point(12, 65);
+            this.dgvConsulta.Name = "dgvConsulta";
+            this.dgvConsulta.Size = new System.Drawing.Size(382, 150);
+            this.dgvConsulta.TabIndex = 1;
             // 
             // btnFechar
             // 
-            this.btnFechar.Location = new System.Drawing.Point(641, 389);
+            this.btnFechar.Location = new System.Drawing.Point(319, 221);
             this.btnFechar.Name = "btnFechar";
-            this.btnFechar.Size = new System.Drawing.Size(147, 49);
+            this.btnFechar.Size = new System.Drawing.Size(75, 32);
             this.btnFechar.TabIndex = 1;
             this.btnFechar.Text = "Fechar";
             this.btnFechar.UseVisualStyleBackColor = true;
             this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
             // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(238, 221);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 32);
+            this.btnImprimir.TabIndex = 2;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // btnGrafico
+            // 
+            this.btnGrafico.Location = new System.Drawing.Point(157, 221);
+            this.btnGrafico.Name = "btnGrafico";
+            this.btnGrafico.Size = new System.Drawing.Size(75, 32);
+            this.btnGrafico.TabIndex = 3;
+            this.btnGrafico.Text = "Gráfico";
+            this.btnGrafico.UseVisualStyleBackColor = true;
+            this.btnGrafico.Click += new System.EventHandler(this.btnGrafico_Click);
+            // 
+            // chartVendas
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chartVendas.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chartVendas.Legends.Add(legend7);
+            this.chartVendas.Location = new System.Drawing.Point(400, 12);
+            this.chartVendas.Name = "chartVendas";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chartVendas.Series.Add(series7);
+            this.chartVendas.Size = new System.Drawing.Size(293, 239);
+            this.chartVendas.TabIndex = 4;
+            this.chartVendas.Text = "chart1";
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
             // FrmRelVendas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(705, 263);
+            this.Controls.Add(this.chartVendas);
+            this.Controls.Add(this.btnGrafico);
+            this.Controls.Add(this.btnImprimir);
             this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.dgvConsulta);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FrmRelVendas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Relatório | Vendas";
+            this.Text = "Relatório de Vendas | Consulta";
+            this.Load += new System.EventHandler(this.FrmRelVendas_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsulta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartVendas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpFinal;
+        private System.Windows.Forms.DateTimePicker dtpInicio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvConsulta;
         private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnGrafico;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartVendas;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
     }
 }
--- FrmRelVendas.cs
+++ FrmRelVendas.cs
@@ -7,14 +7,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+using MySql.Data.MySqlClient;
 
 namespace _23_09_2021
 {
     public partial class FrmRelVendas : Form
     {
+
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        String banco = "server=localhost;port=3306;uid=root;pwd=etecjau;database=ds";
+        MySqlDataAdapter Adaptador;
+        DataTable datTabela;
+
         public FrmRelVendas()
         {
             InitializeComponent();
+        }
+
+        private void FrmRelVendas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            conexao = new MySqlConnection(banco);
+            conexao.Open();
+            comando = new MySqlCommand("select c.nome Categoria, sum(vd.qtde) quantidade from categorias c " +
+                                       "inner join produtos p on (c.id = p.id_categoria) " +
+                                       "inner join venda_det vd on (vd.id_produto = p.id) " +
+                                       "inner join venda_cab vc on (vc.id = vd.id_venda) " +
+                                       "where vc.data >= @dataini and vc.data  <= @datafim group by c.id", conexao);
+            comando.Parameters.AddWithValue("@dataini", DateTime.Parse(dtpInicio.Value.ToString("dd/MM/yyyy")));
+            comando.Parameters.AddWithValue("@datafim", DateTime.Parse(dtpFinal.Value.ToString("dd/MM/yyyy")));
+            Adaptador = new MySqlDataAdapter(comando);
+            Adaptador.Fill(datTabela = new DataTable());
+            dgvConsulta.DataSource = datTabela;
+            conexao.Close();
+        }
+
+        private void btnGrafico_Click(object sender, EventArgs e)
+        {
+            //criação do gráfico através de arrays
+            String[] categoria = new String[dgvConsulta.RowCount];
+            double[] qtde = new double[dgvConsulta.RowCount];
+            int i = 0;
+
+            foreach (DataGridViewRow linha in dgvConsulta.Rows)
+            {
+                categoria[i] = linha.Cells[0].Value.ToString();
+                qtde[i] = Convert.ToDouble(linha.Cells[1].Value);
+                i += 1;
+            }
+            chartVendas.Series[0].ChartType = SeriesChartType.Pie;
+            chartVendas.Titles.Add("Gráfico de Vendas por Categoria");
+            chartVendas.ChartAreas[0].Area3DStyle.Enable3D = true;
+            chartVendas.Series[0].Points.DataBindXY(categoria, qtde);
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            //configura a janela de visualização do modo maximizado
+            printPreviewDialog1.WindowState = FormWindowState.Maximized;
+
+            //abre a tela de visualização do relatório
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            //desenvolvimento do cabeçalho do relatório
+            e.Graphics.DrawString("Reltório de Vendas", new Font("Arial", 30, FontStyle.Bold), Brushes.Black, 230, 10);
+            e.Graphics.DrawLine(Pens.Black, 100, 90, 720, 90);
+            e.Graphics.DrawString("Categoria", new Font("Arial", 10), Brushes.Black, 100, 95);
+            e.Graphics.DrawString("Quantidade", new Font("Arial", 10), Brushes.Black, 600, 95);
+            e.Graphics.DrawLine(Pens.Black, 100, 120, 720, 120);
+
+            //desenvolvimento da interface do corpo do relatorio 
+            int posicao_y = 100;
+            foreach (DataGridViewRow linha in dgvConsulta.Rows)
+            {
+                posicao_y += 30;
+                e.Graphics.DrawString(linha.Cells[0].Value.ToString(), new Font("Arial", 10), Brushes.Black, 100, posicao_y);
+                e.Graphics.DrawString(linha.Cells[1].Value.ToString(), new Font("Arial", 10), Brushes.Black, 600, posicao_y);
+            }
+
+            //desenvolvimento da interface do rodapé do relatório
+            e.Graphics.DrawLine(Pens.Black, 100, 1060, 720, 1060);
+            e.Graphics.DrawString("Total de Categorias", new Font("Arial", 10), Brushes.Black, 100, 1065);
+            e.Graphics.DrawString(dgvConsulta.RowCount.ToString(), new Font("Arial", 10), Brushes.Black, 250, 1065);
+            e.Graphics.DrawString(System.DateTime.Now.ToString(), new Font("Arial", 10), Brushes.Black, 500, 1065);
+            e.Graphics.DrawLine(Pens.Black, 100, 1090, 720, 1090);
         }
 
         private void btnFechar_Click(object sender, EventArgs e)
